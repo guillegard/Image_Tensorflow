c5e3c8e12ce7d47f1ab0d788cc63d91b4955bb3a
guillegard <guillegard95@gmail.com>
2019-04-25 12:29:21 -0500

    first commit

--- scripts/label_image.py
+++ scripts/label_image.py
@@ -131,7 +131,8 @@ if __name__ == "__main__":
   top_k = results.argsort()[-5:][::-1]
   labels = load_labels(label_file)
 
-  print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
+  #print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
   template = "{} (score={:0.5f})"
   for i in top_k:
     print(template.format(labels[i], results[i]))
+    break
