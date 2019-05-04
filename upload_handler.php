dde5c1971a2dcd294146e83112fd5ac30307a30a
guillegard <guillegard95@gmail.com>
2019-05-04 13:07:50 -0500

    test 2 google upload

---
+++ upload_handler.php
@@ -0,0 +1,16 @@
+<?php
+  $gs_name = $_FILES["fileToUpload"]["tmp_name"]; 
+  $fileType = $_FILES["fileToUpload"]["type"]; 
+  $fileSize = $_FILES["fileToUpload"]["size"]; 
+  $fileErrorMsg = $_FILES["fileToUpload"]["error"]; 
+  $fileExt = pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
+
+  // change name if you want
+  $fileName = 'temp.jpg';
+
+  // put to cloud storage
+  $image = file_get_contents($gs_name);
+  $options = [ "gs" => [ "Content-Type" => "image/jpeg"]];
+  $ctx = stream_context_create($options);
+  file_put_contents("gs://img-tellme/".$fileName, $gs_name, 0, $ctx);
+?>
