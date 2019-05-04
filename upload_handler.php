422949c3cde30db76f75d8380ee57c5090e3ec7b
guillegard <guillegard95@gmail.com>
2019-05-04 13:39:46 -0500

    test 6

--- upload_handler.php
+++ upload_handler.php
@@ -1,19 +1,16 @@
+
 <?php
-  require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
-  use google\appengine\api\cloud_storage\CloudStorageTools;
-  
-  $gs_name = $_FILES["fileToUpload"]["tmp_name"]; 
-  $fileType = $_FILES["fileToUpload"]["type"]; 
-  $fileSize = $_FILES["fileToUpload"]["size"]; 
-  $fileErrorMsg = $_FILES["fileToUpload"]["error"]; 
-  $fileExt = pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION);
-
-  // change name if you want
-  $fileName = 'temp.jpg';
-
-  // put to cloud storage
-  $image = file_get_contents($gs_name);
-  $options = [ "gs" => [ "Content-Type" => "image/jpeg"]];
-  $ctx = stream_context_create($options);
-  file_put_contents("gs://img-tellme/".$fileName, $gs_name, 0, $ctx);
+require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+use google\appengine\api\cloud_storage\CloudStorageTools;
+$options = [ 'img-tellme' => 'upload-app' ];
+$upload_url = CloudStorageTools::createUploadUrl('/gcsprocess', $options);
+   $filename = $_FILES['fileToUpload']['name'];
+   
+   $gs_name = $_FILES['fileToUpload']['tmp_name'];
+   move_uploaded_file($gs_name, 'gs://upload-app/'.$filename.'');
+if(isset($_POST['Require']) && $_POST['Require'] === "UploadURL" ){
+	
+	echo $upload_url;	
+	
+}
 ?>
